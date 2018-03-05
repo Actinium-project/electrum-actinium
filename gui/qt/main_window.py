d0f8398df6226b0bbf5f57e058f7b6d0825f5bff
Yura Pakhuchiy <pakhuchiy@gmail.com>
2018-03-05 15:54:51 +0700

    Allow free transactions

--- gui/qt/main_window.py
+++ gui/qt/main_window.py
@@ -1093,12 +1093,7 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, PrintError):
 
         def on_fee_or_feerate(edit_changed, editing_finished):
             edit_other = self.feerate_e if edit_changed == self.fee_e else self.fee_e
-            if editing_finished:
-                if not edit_changed.get_amount():
-                    # This is so that when the user blanks the fee and moves on,
-                    # we go back to auto-calculate mode and put a fee back.
-                    edit_changed.setModified(False)
-            else:
+            if not editing_finished:
                 # edit_changed was edited just now, so make sure we will
                 # freeze the correct fee setting (this)
                 edit_other.setModified(False)
