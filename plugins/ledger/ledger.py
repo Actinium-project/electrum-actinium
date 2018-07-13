rootSig/electrum-atom

ledger: newer fw throws different exception when not in bitcoin mode

plugins/ledger/ledger.py
@@ -180,8 +180,8 @@ class Ledger_Client():
             try:
                 self.perform_hw1_preflight()
             except BTChipException as e:
-                if (e.sw == 0x6d00):
-                    raise BaseException("Device not in Bitcoin mode")
+                if (e.sw == 0x6d00 or e.sw == 0x6f00):
+                    raise BaseException(_("Device not in Bitcoin mode")) from e
                 raise e
             self.preflightDone = True
 

Actinium-project/electrum-actinium

plugins/ledger/ledger.py
@@ -187,8 +187,8 @@ class Ledger_Client():
             try:
                 self.perform_hw1_preflight()
             except BTChipException as e:
-                if (e.sw == 0x6d00):
-                    raise BaseException("Device not in Zcoin mode")
+                if (e.sw == 0x6d00 or e.sw == 0x6700):
+                    raise BaseException(_("Device not in Zcoin mode")) from e
                 raise e
             self.preflightDone = True
 